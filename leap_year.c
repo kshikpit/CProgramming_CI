4e6966b8d192f67d74b864c52611f54edc79391a
kshikpit <Kshiteej.Pathak@kpit.com>
2025-09-30 00:20:31 +0530

    Update leap_year.c

--- leap_year.c
+++ leap_year.c
@@ -6,9 +6,9 @@ int main()
   scanf("%d",&year);
 
   if((year % 4 == 0 && year % 100 != 0)||(year % 400 == 0))
-    printf("\n%d is a leap year");
+    printf("\n%d is a leap year",year);
   else
-    printf("\n%d is not a leap year");
+    printf("\n%d is not a leap year",year);
 
   return 0;
 }
