e0ce30f2ad15648b666843cf18b49cc0efb406dc
kshikpit <Kshiteej.Pathak@kpit.com>
2025-09-30 00:09:33 +0530

    Create fibo.c

---
+++ fibo.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter the number: ");
+  scanf("%d",&n);
+  int a=0,b=1, next;
+
+  printf("\nFibonacci Series:\n");
+  for(int i=0;i<n;i++)
+  {
+    if (i<=1)
+      next=i;
+    else
+    {
+      next=a+b;
+      a=b;
+      b=next;
+    }
+    printf("%d ",next);
+  }
+
+  return 0;
+}
