dca2ab70c434a49cae81cb82739fd32ec39c17d8
kshikpit <Kshiteej.Pathak@kpit.com>
2025-09-30 00:03:09 +0530

    Create leap_year.c

---
+++ leap_year.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int year;
+  printf("Enter the year: ");
+  scanf("%d",&year);
+
+  if((year % 4 == 0 && year % 100 != 0)||(year % 400 == 0))
+    printf("\n%d is a leap year");
+  else
+    printf("\n%d is not a leap year");
+
+  return 0;
+}
